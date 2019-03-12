646fcc304291171b4687e5d8a8c231e459f76235
João Henrique <joaoh3326@gmail.com>
2019-03-12 16:03:06 -0300

    Revert "finalizado desafio"
    This reverts commit b0cd8203de772322848df949eafbe5ac791fa85b.

--- src/index.js
+++ src/index.js
@@ -7,54 +7,13 @@ import Post from './Post';
 import './style.scss';
 
 class App extends Component {
-  state = {
-    posts: [
-      {
-        id: 1,
-        name: 'Diego Schell Fernandes',
-        timePost: '3 min',
-        avatar: './avatar.png',
-        comment: `Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Nullam pharetra egestas aliquam.
-          Morbi sit amet viverra metus. Aliquam erat volutpat. Nam lobortis,
-          nisi vel tincidunt ornare, purus tellus congue neque, ut mollis orci
-          tellus eget quam.`,
-      },
-      {
-        id: 2,
-        name: 'Diego Schell Fernandes',
-        timePost: '3 min',
-        avatar: './avatar.png',
-        comment: `Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Nullam pharetra egestas aliquam.
-          Morbi sit amet viverra metus. Aliquam erat volutpat. Nam lobortis,
-          nisi vel tincidunt ornare, purus tellus congue neque, ut mollis orci
-          tellus eget quam.`,
-      },
-      {
-        id: 3,
-        name: 'Diego Schell Fernandes',
-        timePost: '3 min',
-        avatar: './avatar.png',
-        comment: `Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Nullam pharetra egestas aliquam.
-          Morbi sit amet viverra metus. Aliquam erat volutpat. Nam lobortis,
-          nisi vel tincidunt ornare, purus tellus congue neque, ut mollis orci
-          tellus eget quam.`,
-      },
-    ],
-  };
-
   render() {
-    const { posts } = this.state;
     return (
       <Fragment>
         <Header />
         <div className="wrapper">
           <div className="content">
-            {posts.map(post => (
-              <Post key={post.id} post={post} />
-            ))}
+            <Post />
           </div>
         </div>
       </Fragment>
